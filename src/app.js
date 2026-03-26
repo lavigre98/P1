7bcb72cdb89a91f14015415ed09e6967314631a4
Lara <laravi.gr@gmail.com>
2026-03-26 20:28:37 +0100

    Pàgina acabada

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
-const name = 'world';
-console.log(`Hello ${name}`);
+import confetti from 'canvas-confetti';
+
+// Es llança automàticament en carregar el script
+confetti({
+    particleCount: 150,
+    spread: 70,
+    origin: { y: 0.6 }
+});
+
+// Busquem el títol que ja tenim a l'HTML
+const titol = document.getElementById('titolQueCanviaDeColor');
+
+if (titol) {
+    titol.addEventListener('click', () => {
+        // Al fer clic, el títol canvia de color
+        titol.style.color = '#e67e22'; 
+        console.log("Has clicat el títol!");
+    });
+}
